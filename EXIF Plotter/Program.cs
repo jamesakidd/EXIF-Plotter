871d16c246316f1d27b32dec886c893557d4a409
James Kidd <garlicraspberry@hotmail.com>
2021-09-15 14:04:06 -0400

    Added Error handling

--- EXIF Plotter/Program.cs
+++ EXIF Plotter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
@@ -8,20 +9,19 @@ namespace EXIF_Plotter
 {
     class Program
     {
-
         public static double convertGeoFormat(double[] geoArr)
         {
             return Math.Round(Math.Sign(geoArr[0]) * Math.Abs(geoArr[0] + geoArr[1] / 60.0 + geoArr[2] / 3600.0), 6);
         }
 
-        public static string parseLatLong(string file)
+        public static string parseLatLong(string file, ref List<string> errors)
         {
             string retVal = null;
-
-            using (ExifReader reader = new ExifReader(file))
+            try
             {
-                try
+                using (ExifReader reader = new ExifReader(file))
                 {
+
                     if (reader.GetTagValue<double[]>(ExifTags.GPSLatitude, out var lat))
                     {
                         retVal = convertGeoFormat(lat).ToString(CultureInfo.CurrentCulture);
@@ -31,40 +31,66 @@ namespace EXIF_Plotter
                     {
                         retVal += $",-{convertGeoFormat(lon).ToString(CultureInfo.CurrentCulture)}";
                     }
+
                 }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
+            }
+            catch (ExifLibException)
+            {
+                errors.Add($"No EXIF data found in {file.Substring(file.IndexOf(@"\", StringComparison.Ordinal))}");
+            }
+            catch (Exception ex)
+            {
+                errors.Add(ex.Message);
             }
             return retVal;
         }
 
         static void Main(string[] args)
         {
+            List<string> errors = new List<string>();
             var files = Directory.GetFiles(@"pics\");
-            var coordsList = files.Select(parseLatLong).ToList();
+            var coordsList = new Dictionary<string, string>();
 
-            string uri = $"https://www.google.ca/maps/dir/";
-
-            var last = coordsList.Last();
-            foreach (string coord in coordsList)
+            foreach (string file in files)
             {
-                if (coord.Equals("0,-0"))
+                var tempCoord = parseLatLong(file, ref errors);
+                if (tempCoord != null)
                 {
-                    continue;
-                }
-
-                if (coord.Equals(last))
-                {
-                    uri += $"/@{coord},17.5z";
-                }
-                else
-                {
-                    uri += coord + "/"; 
+                    coordsList.Add(tempCoord, file);
                 }
             }
-            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo { UseShellExecute = true, FileName = uri });
+
+            string uri = $"https://www.google.ca/maps/dir/";
+            if (coordsList.Any())
+            {
+                var last = coordsList.Last();
+
+                foreach (var coord in coordsList)
+                {
+                    if (coord.Key.Equals("0,-0"))
+                    {
+                        errors.Add($"No GPS data found in {coord.Value.Substring(coord.Value.IndexOf(@"\", StringComparison.Ordinal))}");
+                        continue;
+                    }
+
+                    if (coord.Equals(last))
+                    {
+                        uri += $"/@{coord.Key},17.5z";
+                    }
+                    else
+                    {
+                        uri += coord.Key + "/";
+                    }
+                }
+
+                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+                { UseShellExecute = true, FileName = uri });
+            }
+
+            if (!errors.Any()) return;
+            errors.ForEach(Console.WriteLine);
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
